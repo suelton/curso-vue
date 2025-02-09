944d98f6233bfc9edf52df99d63792ecce7e477e
suelton.teixeira <suelton.teixeira@me.com.br>
2025-02-09 18:26:55 -0300

    Adiciona estrutura inicial da aplicação Vue.js com configuração do Babel, Vuetify e arquivos principais

---
+++ vue-cli/projeto-plugins/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <hello-world />
+</template>
+
+<script>
+/* eslint-disable */
+import HelloWorld from '../components/HelloWorld'
+
+  export default {
+    name: 'Home',
+
+    components: {
+      HelloWorld,
+    },
+  }
+</script>
--- vue-cli/projeto-simples/src/App.vue
+++ vue-cli/projeto-simples/src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{ titulo}}
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data: function() {
+    return {
+      titulo: 'Teste Data Usando Vue JS !!!'
+    }
+  }  
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: chocolate;
+  color: #fff;
 }
 </style>
